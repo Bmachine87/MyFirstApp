8d5d11af388ab5f73e6fb3fea51a0dd15d72772e
Benjamin Lee <benjamin.lt.lee@gmail.com>
2017-06-28 23:12:56 -0700

    added ListActivityBen and activity_listviewben.xml to the project

---
+++ app/src/main/java/com/example/kzheng/myfirstapp/ListActivityBen.kt
@@ -0,0 +1,17 @@
+package com.example.kzheng.myfirstapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by blee on 6/28/17.
+ */
+
+class ListActivityBen: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listben)
+    }
+
+    //TODO: Review listviews on android docs + class
+}
